2ce8bdcfb1abb9b651e49fe83f9e0ce8f597d155
raigonlab <rrvnna@gmail.com>
2026-09-08 18:22:33 +0200

    Add black & white visual identity CSS
    Card-grid layout for Collections/Artworks, rounded corners, generous
    whitespace, wordmark-style nav. Fixed a STORAGES misconfiguration
    that broke file uploads after adding WhiteNoise for static files.

--- raigonos/settings.py
+++ raigonos/settings.py
@@ -135,8 +135,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STORAGES = {
+    'default': {
+        'BACKEND': 'django.core.files.storage.FileSystemStorage',
+    },
     'staticfiles': {
         'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
     },
