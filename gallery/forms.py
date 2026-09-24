0f9ec09b377394f13699c4e9bb6a871b27de910c
raigonlab <rrvnna@gmail.com>
2026-09-24 11:03:00 +0200

    Edit forms: show the current image as a thumbnail

--- gallery/forms.py
+++ gallery/forms.py
@@ -3,13 +3,22 @@ from django import forms
 from .models import Artwork, Collection
 
 
+class ImagePreviewInput(forms.ClearableFileInput):
+    """File input that shows the current image as a thumbnail, instead of
+    Django's default "Currently: <path>" text."""
+
+    template_name = 'gallery/widgets/image_preview_input.html'
+
+
 class CollectionForm(forms.ModelForm):
     class Meta:
         model = Collection
         fields = ['title', 'description', 'cover_image', 'status']
+        widgets = {'cover_image': ImagePreviewInput}
 
 
 class ArtworkForm(forms.ModelForm):
     class Meta:
         model = Artwork
         fields = ['title', 'image', 'medium', 'year', 'description', 'display_order']
+        widgets = {'image': ImagePreviewInput}
